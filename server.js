1f9911c942bf7f08f078872d09c6215ab38fa542
cyberdragonz <131214540+cyberdragonz@users.noreply.github.com>
2026-09-16 22:44:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -55,10 +55,8 @@ function createNimRequest(body, nimModel) {
 
   // thinking mode can freeze some DeepSeek variants
   if (ENABLE_THINKING_MODE) {
-    request.extra_body = {
-      chat_template_kwargs: {
+    request.chat_template_kwargs: {
         thinking: true
-      }
     };
   }
 
